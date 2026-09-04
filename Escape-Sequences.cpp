e23046805d83ef4d675206c830bb74e6ac12f851
Zain Ul Arifeen <zayn.ul.arifeen2412@gmail.com>
2026-09-04 17:42:58 +0500

    Update Escape-Sequences.cpp

--- Escape-Sequences.cpp
+++ Escape-Sequences.cpp
@@ -27,7 +27,7 @@ int main() {
 
     // \r moves the cursor back to the beginning of the line
     // The text after \r can overwrite the text already printed
-    cout << "12345\rAB\n";
+    cout << "1234EFGH\r5678";
 
     // endl also moves to a new line but it requires the stream insertion operator
     cout << "This is line one." << endl;
