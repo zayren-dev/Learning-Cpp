589beba6435c81d5b638c7a891a5bfcf7bcb12c9
Zain Ul Arifeen <zayn.ul.arifeen2412@gmail.com>
2026-09-08 20:20:30 +0500

    Update input_output

--- input_output.cpp
+++ input_output.cpp
@@ -2,10 +2,14 @@
 #include<iostream>
 int main()
 {
+    double x;
     int number; // here we declared a variable called number of type int
     std::cout << "Enter a number: "; // here we printed a message to the console asking the user to enter a number
     std::cin >> number; // here we took input from the user and stored it in the variable number
     std::cout << "You entered: " << number; // here we printed the value of number to the console
+    std::cout << "\n" << "Enter a floating point number: "; // here we printed a new message to the console asking the user to enter a number
+    std::cin >> x; // here we took input from the user and stored it in the variable x
+    std::cout << "You entered: " << x; // here we printed the value of x to the console
   return 0;
 
 }
