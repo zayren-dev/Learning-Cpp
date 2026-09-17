f4c162368651ea491808979372bfdcedc8b7eed5
Zain Ul Arifeen <zayn.ul.arifeen2412@gmail.com>
2026-09-17 21:26:53 +0500

    Change input_output.cpp to Practice.cpp

---
+++ Practice.cpp
@@ -0,0 +1,17 @@
+/* This is a simple C++ program that demonstrates how to perform if-else work without actually
+using ifs , logical and relational operators. */
+#include<iostream>
+using namespace std;
+int main()
+{
+  int fee = 150001;
+  int a = fee/(150001);
+
+  int cond = (2*a)/(a+1);
+
+  int tax = cond* (fee * 0.1) + (1 - cond) * (fee * 0.05);
+  cout << "tax " << tax << endl;
+  cout << "The condition is " << cond << endl;
+  return 0;
+
+}
