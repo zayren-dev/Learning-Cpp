99fc35dae5ec05319b048ba0277cee00b34c6a5b
Zain Ul Arifeen <zayn.ul.arifeen2412@gmail.com>
2026-09-20 13:42:10 +0500

    Update practice2.cpp

--- practice2.cpp
+++ practice2.cpp
@@ -7,7 +7,13 @@ int main()
     cout << "Dry Run 1 "<< endl;
     unsigned short x = 65500;
     x = x + 100;
-    cout << x;
+    cout << x<<endl;
+    cout<<"========================================"<<endl;
+    cout<<"Dry Run 2 "<<endl;
+    int z = -5;
+    unsigned int y = z;
+    cout << y<<endl;
+
 
 return 0;
 }
