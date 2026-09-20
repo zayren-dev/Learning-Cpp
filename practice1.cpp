53870d911213cf5d3aa2a7bd4f427793bd99fb22
Zain Ul Arifeen <zayn.ul.arifeen2412@gmail.com>
2026-09-20 11:38:48 +0500

    Update practice1.cpp

--- practice1.cpp
+++ practice1.cpp
@@ -2,6 +2,8 @@
 using namespace std;
 int main()
 {
+    cout << "========================================"<< endl;
+    cout << "Dry Run 1 "<< endl;
     int myvar = 5;
     if(-1){
         cout<< myvar<<endl;
@@ -17,5 +19,8 @@ int main()
         cout<< myvar--<<endl;
     }
     cout<< myvar<<endl;
+    cout<< "========================================"<< endl;
+    cout<< "Dry Run 2 "<< endl;
+    
     return 0;
 }
